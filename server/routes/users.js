50b0699b3cf9dbda8f807172fdf74b3e5f0602ab
Carmen <carmenshiu07@gmail.com>
2024-10-30 17:33:18 -0700

    dev

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,11 @@
 import express from "express";
+import { User } from "../models/user";
 
 const router = express.Router();
 
 router.get('/', (_, res) => {
-    res.json('get works');
-})
+    User.find().then(users => res.json(users))
+        .catch(err => res.status(404));
+});
 
 export const users = router;
