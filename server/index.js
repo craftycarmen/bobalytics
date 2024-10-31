9abd90d33b6f671f7848fbdff1268c9b94a281a7
Carmen <carmenshiu07@gmail.com>
2024-10-30 17:38:05 -0700

    connected mongodb

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import { MONGO_URI } from "./config/keys.js";
 import { users } from "./routes/users.js";
 
 const app = express();
 
 app.use(express.json());
+
+mongoose
+    .connect(MONGO_URI)
+    .then(() => console.log('MongoDB is successfully connected'))
+    .catch(err => console.log(err))
 
 app.get('/test', (_, res) => res.send("This works"));
 
